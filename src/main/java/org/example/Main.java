b76364aa85e39761d82f9010217de501bc2f3861
hoffmann <hoffmann@minova.aero>
2024-11-04 15:29:50 +0100

    hashing data and convert byte[] to hex string and hex string to byte[]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,28 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        String input = "H";
-        String hashValue = DavoHash512.hash(input);
-        System.out.println("Hash-Wert: " + hashValue);
+        String input = "Hi";
+        byte[] hash = DavoHash512.hash(input);
+        System.out.println("Hash: " + DavoHash512.bytesToHex(hash));
+
+
+        String filePath = "C:/Users/hoffmann/Documents/tests.txt";  // Dateipfad anpassen
+
+        // Datei hashen
+        File file = new File(filePath);
+        byte[] fileHash = DavoHash512.hashFile(file);
+
+        // Hash als Hex-String ausgeben
+        String hexHash = DavoHash512.bytesToHex(fileHash);
+        System.out.println("Datei-Hash: " + hexHash);
+
+
     }
 
 }
