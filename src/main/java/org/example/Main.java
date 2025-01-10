e169e41f693645b21ac48c67f9d4eaa9201d2f7e
David <dav.sonstiges@gmail.com>
2025-01-10 15:44:56 +0100

    initial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,21 +7,10 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-      /*  String input = "Hi";
+
+        String input = "Hi";
         byte[] hash = DavoHash512.hash(input);
         System.out.println("Hash: " + DavoHash512.bytesToHex(hash));
-*/
-
-        String filePath = "C:/Users/PC/Documents/test.txt";
-
-        // Datei hashen
-        File file = new File(filePath);
-        byte[] fileHash = DavoHash512.hashFile(file);
-
-        // Hash als Hex-String ausgeben
-        String hexHash = DavoHash512.bytesToHex(fileHash);
-        System.out.println("Datei-Hash: " + hexHash);
-
 
     }
 
